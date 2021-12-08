6d9793b21df04cdcc5715e7f7a186f80a5c7ca67
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-12-08 21:36:14 +0100

    Add basic layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import {AiOutlineMenu} from 'react-icons/ai';
+import {AiOutlineLogout} from 'react-icons/ai';
 import './home.scss';
 
 class Home extends Component {
   render() {
     return (
       <main className='main-wrapper'>
-        <div className='navigation'></div>
-        <div className='side-bar'>
-          
+        <div className='nav'>
+          <h3 className="navbar-brand">Would you Rather?</h3>
+          <AiOutlineMenu className='menu-icon '/>
         </div>
         <div className='wrapper'>
-          
+          <div className='side-bar'>
+            <div className='sidebar-top'>
+              <div className='profile-pic'></div>
+              <div className='option-btns'></div>
+            </div>
+            <div className='sidebar-bottom'><AiOutlineLogout title='Logout' className='logout-icon'/> </div>
+          </div>
+          <div className='content'>
+            
+          </div>
         </div>
       </main>
     )
