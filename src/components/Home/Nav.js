c1cc620b3fdcfee1027963655259990be2007a88
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-12-18 19:47:56 +0100

    Decompose Home component

---
+++ src/components/Home/Nav.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import {AiOutlineMenu} from 'react-icons/ai';
+
+
+
+class Nav extends Component {
+  render(){
+    // console.log('I am a prop in Nav', this.props)
+    return (
+      <div className='nav'>
+        <h3 className="navbar-brand">Would you Rather?</h3>
+        <div className="navbar-menu">
+          <p className="authed-user">{this.props.authedUser? this.props.users[this.props.authedUser]['name'] : 'Welcome' }</p>
+          <AiOutlineMenu className='menu-icon'/>
+        </div>
+      </div>
+    )
+  }
+}
+function mapStateToProps({authedUser, users}){
+  return {
+    users,
+    authedUser,
+  }
+}
+
+export default connect(mapStateToProps)(Nav);
