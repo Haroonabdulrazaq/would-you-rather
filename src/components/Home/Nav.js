7d3bd548f508acd0140f859dc3c7112f85a23a85
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-12-23 15:49:29 +0100

    Add navigation to project

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -2,21 +2,42 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom'
 
-import {AiOutlineMenu} from 'react-icons/ai';
+import {AiOutlineMenu, AiFillCloseCircle} from 'react-icons/ai';
+// import {} from 'react-icons/ai';
 import './nav.scss';
 
 
 class Nav extends Component {
+  state= {
+    open: false
+  }
+  handleClick =()=>{
+    this.setState((prevState)=>({
+      open: !prevState.open,
+    }))
+  }
   render(){
-    // console.log('I am a prop in Nav', this.props)this.props.,authedUser
     return (
-      <div className='nav'>
-        <h3 className="navbar-brand"><Link to='/'>Would <span className='you'>you</span>Rather?</Link></h3>
-        <div className="navbar-menu">
-          <Link to='/leaderboard'>Leaderboard</Link>
-          <p className="authed-user">{this.props.authedUser? this.props.users[this.props.authedUser]['name'] : 'Welcome' }</p>
-          {this.props.authedUser && <AiOutlineMenu className='menu-icon'/>}
+      <div className='nav-parent'>
+        <div className='nav'>
+          <h3 className="navbar-brand"><Link to='/'>Would <span className='you'>you</span>Rather?</Link></h3>
+          <div className="navbar-menu">
+            <p className="authed-user">{this.props.authedUser? this.props.users[this.props.authedUser]['name'] : 'Welcome' }</p>
+            {!this.props.user?
+              'null'
+              : this.state.open?
+              <AiFillCloseCircle className='menu-icon' onClick={this.handleClick}/>:
+              <AiOutlineMenu className='menu-icon' onClick={this.handleClick} />
+            }
+          </div>
         </div>
+        {this.state.open && <div className='nav-container'>
+          <div className='nav-wrapper'>
+            <Link to='/' className='nav-link'>Home</Link>
+            <Link to='/add' className='nav-link'>Add Question</Link>
+            <Link to='/leaderboard' className='nav-link'>Leaderboard</Link>
+          </div>
+        </div>}
       </div>
     )
   }
